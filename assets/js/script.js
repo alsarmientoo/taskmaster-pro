2522b6dbf775d2bf689643f9703f6cc8c00a9c4b
Pepp <reels-fencers.0@icloud.com>
2022-08-01 01:15:14 -0400

    add css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,16 +54,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    // console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", this);
+    // console.log("over", this);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", this);
+    // console.log("out", this);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data
@@ -106,17 +111,20 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui)  {
-    console.log("drop");
+    // console.log("drop");
     // remove dragged item from the dom
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
 
   over: function(event, ui) {
-    console.log("over");
+    // console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   
   out: function(event, ui) {
-    console.log("out");
+    // console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -158,7 +166,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -302,5 +310,14 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+// 
+setInterval(function() {
+  // alert("This message happens after 5 seconds!");
+  $(".card .list-group-item").each(function(index, el) {
+    // In this interval, we loop over every task on the page with a class of list-group-item 
+    // and execute the auditTask() function to check the due date of each one
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 
